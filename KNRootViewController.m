74b1cbddca586117d95e25f50947e79420b73003
freedg <geh485122@gmail.com>
2017-02-13 17:50:51 -0600

    show small thumbnail of most recent image uploaded

--- KNRootViewController.m
+++ KNRootViewController.m
@@ -1,4 +1,5 @@
 #import "KNRootViewController.h"
+#import "Classes/SBJson5.h"
 
 @implementation KNRootViewController {
 	NSMutableArray *_objects;
@@ -11,16 +12,32 @@
 	self.title = @"derpi";
 	self.view = [[[UIView alloc]initWithFrame:[[UIScreen mainScreen] applicationFrame]] autorelease];
 	self.view.backgroundColor = [UIColor whiteColor];
-	
+	/*
 	UILabel *testLabel = [[UILabel alloc] initWithFrame:CGRectMake(10,150,300,20)];
 	[testLabel setText:@"big boot"];	
 	[self.view addSubview:testLabel];
-	
+	*/	
+	/*
 	UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10,300,101,150)];
 	NSData *imageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString: @"https://derpicdn.net/img/2015/4/30/885907/thumb_small.jpg"]];
 	imageView.image = [UIImage imageWithData:imageData];
 	[self.view addSubview:imageView];
+	*/
+	NSData *data = [[NSData alloc]initWithContentsOfURL: [NSURL URLWithString:@"https://derpibooru.org/images.json"]];
+	NSMutableDictionary *s = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+	NSArray *images = [s objectForKey:@"images"];
+	NSDictionary *firstImages = images[0];
+	NSDictionary *representations = firstImages[@"representations"];
 
+	NSData *imageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat: @"https:%@",representations[@"thumb_small"]]]];
+	UIImage *image = [UIImage imageWithData:imageData];
+	UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10,75,image.size.height,image.size.width)];
+	imageView.image = image;
+	[self.view addSubview:imageView];
+
+	UILabel *imageLabel = [[UILabel alloc] initWithFrame:CGRectMake(10,50,300,300)];
+	[imageLabel setText:[NSString stringWithFormat:@"%f x %f",imageView.image.size.width, imageView.image.size.height]];
+	[self.view addSubview:imageLabel];
 }
 
 @end
